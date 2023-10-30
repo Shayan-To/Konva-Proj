68ff2b28778cff6dbc9d46debcc61b3874e98a19
Alwinum <alwinum@Christopher.local>
2023-10-30 18:34:01 +0330

    MESSAGE

--- app/app.ts
+++ app/app.ts
@@ -1,45 +1,62 @@
 import Konva from "konva";
 import { createStage } from "./base";
 import * as piecesSvg__ from "raw-string:../assets/kosal/*.svg";
+import { createSvgImage } from "./base";
 
 const piecesSvg: { [key: string]: string } = piecesSvg__ as any;
 
 type PieceType = "P" | "N" | "B" | "R" | "Q" | "K";
-
-const logicalWidth = 1000;
-const logicalHeight = 1000;
+/*
+P : Pawn, N : Night (knight), B : Bishop, R : Rook, Q : Queen, K : King 
+*/
+const logicalWidth = 800;
+const logicalHeight = 800;
 const cellDim = logicalHeight / 8;
 type Point = readonly [x: number, y: number];
-const XX = 2;
+var firstClick : boolean = false; 
+var secondClick : boolean = false; 
 const stage = createStage(logicalWidth, logicalHeight);
 const layer = new Konva.Layer({});
-stage.add(layer);
+
+stage.add(layer); 
+const timeCheck = 300; 
+const Border = new Konva.Rect({
+    fill:"Black", 
+    width : logicalWidth + 10, 
+    height : logicalHeight + 10, 
+    x : -5, 
+    y : -5, 
+})
+layer.add(Border); 
+
 function BlackOrWhite(x: number, y: number): boolean {
     if ((x % 2 === 0 && y % 2 !== 0) || (x % 2 !== 0 && y % 2 == 0)) {
         return false;
     }
     return true;
 }
+
+
 for (let i = 1; i <= 8; i++) {
     for (let j = 1; j <= 8; j++) {
         createCell(i, j, BlackOrWhite(i, j));
+        
     }
 }
-createPiece(1, 1, false, "B");
 
-function createCell(x: number, y: number, b: boolean): void {
-    const cell = new Konva.Rect({
-        fill: "Black",
-        width: cellDim,
-        height: cellDim,
-        x: cellDim * (x - 1),
-        y: cellDim * (y - 1),
-    });
-    if (b) {
-        cell.fill("Orange");
-    }
-    layer.add(cell);
-}
+// createPiece(1, 1, false, "B");
+// Now we place our pieces ; 
+// false -> Black 
+// true -> white 
+
+
+
+
+// createPiece(1, 1, "false, "R"); 
+var tpe : PieceType; 
+var x1 : number, x2 : number, y1 : number, y2 : number; 
+var blk : boolean; 
+var pce : Konva.Node; 
 async function createPiece(x: number, y: number, b: boolean, p: PieceType) {
     const piece = await createSvgImage(piecesSvg[`${b ? "w" : "b"}${p}`], {
         width: cellDim,
@@ -47,34 +64,84 @@ async function createPiece(x: number, y: number, b: boolean, p: PieceType) {
         x: cellDim * (x - 1),
         y: cellDim * (y - 1),
     });
+    console.log(typeof(piece)); 
     layer.add(piece);
+    piece.on("click", ()=>{
+        firstClick = true; 
+        x1 = x; 
+        y1 = y; 
+        tpe = p; 
+        blk = b; 
+        pce = piece; 
+        // movePiece(piece, cell); 
+    })
 }
 
-function createSvgImage(svgString: string, config: Partial<Konva.ImageConfig>) {
-    const domParser = new DOMParser();
-    const xmlSerializer = new XMLSerializer();
+// cell.on("click", () => cell.fill("blue"));
+// layer.add(cell);
+// foodRect = cell;
 
-    var svgDoc = domParser.parseFromString(svgString, "image/svg+xml");
-    svgDoc.documentElement.setAttribute("width", "50px");
-    svgDoc.documentElement.setAttribute("height", "50px");
 
-    return new Promise<Konva.Image>((resolve, reject) => {
-        var img = document.createElement("img");
-        img.onload = function () {
-            resolve(
-                new Konva.Image({
-                    ...config,
-                    image: img,
-                })
-            );
-        };
-        img.onerror = (event, source, lineno, colno, error) =>
-            reject({ event, source, lineno, colno, error });
-        img.crossOrigin = "Anonymous";
-        img.src = URL.createObjectURL(
-            new Blob([xmlSerializer.serializeToString(svgDoc)], {
-                type: "image/svg+xml",
-            })
-        );
+function piecePlace() : void {
+    for (let i = 1; i <= 8; i ++) {
+        createPiece(i, 2, false, "P"); 
+        createPiece(i, 7, true, "P"); 
+    }
+    createPiece(1, 1, false, "R");
+    createPiece(8, 1, false, "R");  
+    createPiece(1, 8, true, "R"); 
+    createPiece(8, 8, true, "R"); 
+    
+    createPiece(2, 1, false, "N"); 
+    createPiece(7, 1, false, "N"); 
+    createPiece(2, 8, true, "N"); 
+    createPiece(7, 8, true, "N"); 
+    
+    createPiece(3, 1, false, "B"); 
+    createPiece(6, 1, false, "B"); 
+    createPiece(3, 8, true, "B"); 
+    createPiece(6, 8, true, "B"); 
+    
+    createPiece(4, 1, false, "Q"); 
+    createPiece(4, 8, true, "Q"); 
+    
+    createPiece(5, 1, false, "K"); 
+    createPiece(5, 8, true, "K"); 
+    
+}
+
+piecePlace (); 
+
+function createCell(x: number, y: number, b: boolean): void {
+    const cell = new Konva.Rect({
+        fill: "Blue",
+        width: cellDim ,
+        height: cellDim ,
+        x: (cellDim ) * (x - 1),
+        y: (cellDim ) * (y - 1),
     });
+    if (b) {
+        cell.fill("white");
+    }
+    layer.add(cell);
+    cell.on("click",()=>{
+        if(!firstClick) {
+            console.log(`First you must choose a piece to move`); 
+        }
+        else {
+            secondClick = true; 
+            x2 = x; 
+            y2 = y; 
+            
+            pieceMove(); 
+        }
+
+    } )
+
+}
+function pieceMove() : void {
+    pce.remove(); 
+    createPiece(x2, y2, blk, tpe);
+    firstClick = false; 
+    secondClick = false;  
 }
--- app/mycode.ts
+++ app/mycode.ts
@@ -1,184 +1,44 @@
 import Konva from "konva";
 import { createStage } from "./base";
+import * as piecesSvg__ from "raw-string:../assets/kosal/*.svg";
+import { createSvgImage } from "./base";
 
-const logicalWidth = 1000;
-const logicalHeight = 1000;
+const piecesSvg: { [key: string]: string } = piecesSvg__ as any;
 
+type PieceType = "P" | "N" | "B" | "R" | "Q" | "K";
+
+const logicalWidth = 800;
+const logicalHeight = 800;
+const cellDim = logicalHeight / 8;
 type Point = readonly [x: number, y: number];
-
+var firstclick : boolean = false; 
+var secondclick : boolean = false; 
 const stage = createStage(logicalWidth, logicalHeight);
 const layer = new Konva.Layer({});
-stage.add(layer);
-const back = new Konva.Rect({
-    fill: "lightblue",
-    width: logicalWidth,
-    height: logicalHeight,
-});
-layer.add(back);
+stage.add(layer); 
+const Border = new Konva.Rect({
+    fill:"Black", 
+    width : logicalWidth + 10, 
+    height : logicalHeight + 10, 
+    x : -5, 
+    y : -5, 
+})
+layer.add(Border); 
 
-const cellDim = 100;
-const gridWidth = logicalWidth / cellDim;
-const gridHeight = logicalHeight / cellDim;
-
-const bodyRects: Konva.Rect[] = [];
-const bodyQueue: Point[] = [];
-let foodRect: Konva.Rect | null = null;
-
-let directionQueue: Point[] = [[1, 0]];
-let head = negate(directionQueue[0]);
-let food = makePoint();
-
-const minMoveDelay = 100; //ms
-const maxMoveDelay = 400; //ms
-let moveDelay = maxMoveDelay;
-
-let version = 0;
-
-async function main() {
-    version += 1;
-    const currentVersion = version;
-
-    bodyRects.splice(0).forEach((r) => r.remove());
-    bodyQueue.splice(0);
-    foodRect?.remove()
-    directionQueue = [[1, 0]];
-    head = negate(directionQueue[0]);
-    moveDelay = maxMoveDelay;
-
-    for (const _ of new Array(3).fill(0).map((_, i) => i)) {
-        moveHead();
-        createCell(head);
-        bodyQueue.push(head);
-        await delay(moveDelay);
-    }
-
-    createFood();
-
-    while (version === currentVersion) {
-        moveHead();
-        bodyQueue.push(head);
-        createCell(head);
-
-        if (!equals(food, head)) {
-            bodyRects.splice(0, 1)[0].remove();
-            bodyQueue.splice(0, 1);
-        } else {
-            createFood();
-            moveDelay -= moveDelay * 7/100;
-            moveDelay = Math.max(minMoveDelay, moveDelay);
-        }
-
-        if (
-            bodyQueue.find(
-                (p, i) => i !== bodyQueue.length - 1 && equals(p, head)
-            )
-        ) {
-            back.fill("red");
-            console.log(`Game over`);
-            break;
-        }
-
-        await delay(moveDelay);
-    }
-}
-
-main();
-
-document.onkeydown = (e) => {
-    let newDirection: Point | undefined;
-    console.log(e.key);
-    switch (e.key) {
-        case "ArrowLeft":
-            newDirection = [-1, 0];
-            break;
-        case "ArrowRight":
-            newDirection = [1, 0];
-            break;
-        case "ArrowUp":
-            newDirection = [0, -1];
-            break;
-        case "ArrowDown":
-            newDirection = [0, 1];
-            break;
-        case "r":
-        case "R":
-            main();
-            break;
-    }
-    if (newDirection === undefined) {
-        return;
-    }
-    while (directionQueue.length > 2) {
-        directionQueue.pop();
-    }
-    if (
-        !equals(newDirection, negate(directionQueue[directionQueue.length - 1]))
-    ) {
-        directionQueue.push(newDirection);
-    }
-};
-
-function delay(ms: number) {
-    return new Promise((resolve) => setInterval(resolve, ms));
-}
-
-function add([x1, y1]: Point, [x2, y2]: Point): Point {
-    return [x1 + x2, y1 + y2];
-}
-
-function negate([x, y]: Point): Point {
-    return [-x, -y];
-}
-
-function mod([x1, y1]: Point, [x2, y2]: Point): Point {
-    return [((x1 % x2) + x2) % x2, ((y1 % y2) + y2) % y2];
-}
-
-function equals([x1, y1]: Point, [x2, y2]: Point): boolean {
-    return x1 === x2 && y1 === y2;
-}
-
-function moveHead() {
-    if (directionQueue.length > 1) {
-        directionQueue.splice(0, 1);
-    }
-    head = mod(add(head, directionQueue[0]), [gridWidth, gridHeight]);
-}
-
-function makePoint(): Point {
-    const x = Math.floor(Math.random() * gridWidth);
-    const y = Math.floor(Math.random() * gridHeight);
-    return [x, y];
-}
-
-function createCell(p: Point) {
-    const cell = createRect(p, "black");
-    layer.add(cell);
-    bodyRects.push(cell);
-}
-
-function createFood() {
-    foodRect?.remove();
-    foodRect = null;
-    while (true) {
-        food = makePoint();
-        if (!bodyQueue.find((p) => equals(food, p))) {
-            break;
-        }
-    }
-
-    const cell = createRect(food, "purple");
-    cell.on("click", () => cell.fill("blue"));
-    layer.add(cell);
-    foodRect = cell;
-}
-
-function createRect([x, y]: Point, color: string) {
-    return new Konva.Rect({
-        fill: color, //`hsl(${Math.random() * 360}, 100%, 50%)`,
+async function createPiece(x: number, y: number, b: boolean, p: PieceType) {
+    const piece = await createSvgImage(piecesSvg[`${b ? "w" : "b"}${p}`], {
         width: cellDim,
         height: cellDim,
-        x: x * cellDim,
-        y: y * cellDim,
+        x: cellDim * (x - 1),
+        y: cellDim * (y - 1),
     });
+    console.log(typeof(piece)); 
+    layer.add(piece);
+    piece.on("click", ()=>{
+        firstclick = true; 
+        // movePiece(piece, cell); 
+        piece.remove(); 
+    })
 }
+
+createPiece(1, 1, true, "B"); 
